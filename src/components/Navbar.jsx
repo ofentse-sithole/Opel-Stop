1d73e0c3b2bb035039539a441d95ab877f0f3213
Ofentse Sithole <124260201+ofentse-sithole@users.noreply.github.com>
2025-09-24 11:36:26 +0000

    moved the menu item to middle

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -30,18 +30,6 @@ function Navbar()  {
                             </Link>
                         </li>
 
-                        {/*<li className="nav-item">
-                            <Link to="/services" className="nav-links" onClick={closeMobileMenu}>
-                                Services
-                            </Link>
-                        </li>
-
-                        <li className="nav-item">
-                            <Link to="/about" className="nav-links" onClick={closeMobileMenu}>
-                                About
-                            </Link>
-                        </li>*/}
-
                         <li className="nav-item">
                             <Link to="/contact" className="nav-links" onClick={closeMobileMenu}>
                                 Contact
